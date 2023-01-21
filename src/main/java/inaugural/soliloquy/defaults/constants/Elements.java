fe08f6ae9c53d0827561aa3572dd964847f654d6
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2023-01-21 00:20:44 -0500

    feat: Add JSON objects and constants for Elements in default ruleset

---
+++ src/main/java/inaugural/soliloquy/defaults/constants/Elements.java
@@ -0,0 +1,46 @@
+package inaugural.soliloquy.defaults.constants;
+
+public class Elements {
+    public final static String COMBUSTION_ID = "combustion";
+    public final static String COMBUSTION_NAME = "Combustion";
+    public final static String COMBUSTION_IMAGE_ASSET_SET_ID = "combustionImageAssetSetId";
+
+    public final static String STEEL_ID = "steel";
+    public final static String STEEL_NAME = "Steel";
+    public final static String STEEL_IMAGE_ASSET_SET_ID = "steelImageAssetSetId";
+
+    public final static String TERRA_ID = "terra";
+    public final static String TERRA_NAME = "Terra";
+    public final static String TERRA_IMAGE_ASSET_SET_ID = "terraImageAssetSetId";
+
+    public final static String ICE_ID = "ice";
+    public final static String ICE_NAME = "Ice";
+    public final static String ICE_IMAGE_ASSET_SET_ID = "iceImageAssetSetId";
+
+    public final static String AQUA_ID = "aqua";
+    public final static String AQUA_NAME = "Aqua";
+    public final static String AQUA_IMAGE_ASSET_SET_ID = "aquaImageAssetSetId";
+
+    public final static String FLORA_ID = "flora";
+    public final static String FLORA_NAME = "Flora";
+    public final static String FLORA_IMAGE_ASSET_SET_ID = "floraImageAssetSetId";
+
+    public final static String AERO_ID = "aero";
+    public final static String AERO_NAME = "Aero";
+    public final static String AERO_IMAGE_ASSET_SET_ID = "aeroImageAssetSetId";
+
+    public final static String CURRENT_ID = "current";
+    public final static String CURRENT_NAME = "Current";
+    public final static String CURRENT_IMAGE_ASSET_SET_ID = "currentImageAssetSetId";
+
+    public final static String[] ELEMENT_IDS = new String[] {
+            COMBUSTION_ID,
+            STEEL_ID,
+            TERRA_ID,
+            ICE_ID,
+            AQUA_ID,
+            FLORA_ID,
+            AERO_ID,
+            CURRENT_ID
+    };
+}
