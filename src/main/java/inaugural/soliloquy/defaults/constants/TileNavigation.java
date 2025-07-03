9d260f16a347df03295294ad015809e9ec5a0f40
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-07-03 15:01:15 -0500

    feat: Add simple constant for tile default movement cost

---
+++ src/main/java/inaugural/soliloquy/defaults/constants/TileNavigation.java
@@ -0,0 +1,10 @@
+package inaugural.soliloquy.defaults.constants;
+
+public class TileNavigation {
+    /**
+     * The number of AP it costs by default to move one space
+     * <p>
+     * Type: int
+     */
+    public final static int DEFAULT_MOVEMENT_COST = 1;
+}
